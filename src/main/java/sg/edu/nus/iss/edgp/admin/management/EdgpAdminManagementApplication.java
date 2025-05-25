f0a84eb6e7ba4aa94f520d21e5001405ce9ce562
Thet-Nandar-Aung <e1327955@u.nus.edu>
2025-05-25 14:15:02 +0800

    Initial commit: backend setup with database config, AWS secret loading, and security starter.

---
+++ src/main/java/sg/edu/nus/iss/edgp/admin/management/EdgpAdminManagementApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.edgp.admin.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgpAdminManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgpAdminManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/edgp/admin/management/EdgpAdminManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.edgp.admin.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgpAdminManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
